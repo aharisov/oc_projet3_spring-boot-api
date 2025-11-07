a01bf39c884b4d168befc928b632f9c2b850bd5b
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-07 17:24:48 +0100

    added GlobalExceptionHandler class

---
+++ src/main/java/com/openclassrooms/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(Exception.class) 
+    public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
+        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST) 
+                .body(error);
+    }
+}
