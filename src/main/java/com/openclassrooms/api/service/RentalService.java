76dd7673752d758c45bcc6ce57ae965d15f4a4c5
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-13 16:48:56 +0100

    feat: :sparkles: Added RentalDTO and methode convertToRental in RentalService

--- src/main/java/com/openclassrooms/api/service/RentalService.java
+++ src/main/java/com/openclassrooms/api/service/RentalService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.openclassrooms.api.dto.RentalDto;
 import com.openclassrooms.api.model.Rental;
 import com.openclassrooms.api.repository.RentalRepository;
 
@@ -16,6 +17,8 @@ public class RentalService {
 	
 	@Autowired
 	private final RentalRepository rentalRepository;
+	@Autowired
+	private FileService fileService;
 	
 	public void addRental(Rental rental) {
 		rentalRepository.save(rental);
@@ -27,5 +30,24 @@ public class RentalService {
 	
 	public Optional<Rental> getRentalById(Integer id) {
 		return rentalRepository.findById(id);
+	}
+	
+	public void updateRental(Rental rental) {
+		if (rentalRepository.findById(rental.getId()) != null) {
+			rentalRepository.save(rental);
+		}
+	}
+	
+	public Rental convertToRental(RentalDto rentalDto) {
+		// create new instance of rental object
+		Rental rental = new Rental();
+		
+		// add data to the object
+		rental.setName(rentalDto.getName());
+		rental.setSurface(rentalDto.getSurface());
+		rental.setPrice(rentalDto.getPrice());
+		rental.setDescription(rentalDto.getDescription());
+		
+		return rental;
 	}
 }
