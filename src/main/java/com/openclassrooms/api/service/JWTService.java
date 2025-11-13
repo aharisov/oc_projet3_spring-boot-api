9a894ae7d91532b4a6a09a20baaa1320b1a6f1c1
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-13 12:05:06 +0100

    feat: :sparkles: Added method for JWT decode in JWTService

--- src/main/java/com/openclassrooms/api/service/JWTService.java
+++ src/main/java/com/openclassrooms/api/service/JWTService.java
@@ -5,7 +5,9 @@ import java.time.temporal.ChronoUnit;
 
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
@@ -15,9 +17,11 @@ import com.openclassrooms.api.model.User;
 @Service
 public class JWTService {
 	private JwtEncoder jwtEncoder;
+	private JwtDecoder jwtDecoder;
     
-    public JWTService(JwtEncoder jwtEncoder) {
+    public JWTService(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder) {
         this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
     }
     
     public String generateToken(User user) {
@@ -29,7 +33,7 @@ public class JWTService {
         		.issuer("self")
         		.issuedAt(now)
         		.expiresAt(now.plus(1, ChronoUnit.DAYS))
-        		.subject(user.getEmail() + user.getName())
+        		.subject(user.getEmail())
         		.build();
         
     	// add params for token generation    	
@@ -37,5 +41,10 @@ public class JWTService {
         
         // return token        
         return this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+    }
+    
+    public Jwt decodeToken(String token) {
+    	// simply call decode methode from security config and send decoded token info    	
+    	return jwtDecoder.decode(token);
     }
 }
