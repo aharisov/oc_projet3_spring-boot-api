43c8e4236f6a4dc94aebcdc4016b312ce6706ea8
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-14 13:48:20 +0100

    refactor: :recycle: Added annotations for swagger in Message model

--- src/main/java/com/openclassrooms/api/model/Message.java
+++ src/main/java/com/openclassrooms/api/model/Message.java
@@ -5,7 +5,11 @@ import java.time.Instant;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,22 +18,35 @@ import lombok.Data;
 public class Message {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Schema(description = "Message ID", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Integer id;
 	
 	@Column(length = 2000)
+	@NotBlank
+	@NotNull
+	@Size(max = 2000)
+    @Schema(description = "Message text", example = "Mon message")
 	private String message;
 	
+	@NotBlank
+	@NotNull
 	@Column(name = "user_id", nullable = false)
+	@Schema(description = "ID of the message owner", example = "1")
 	private Integer user_id;
 	
+	@NotBlank
+	@NotNull
 	@Column(name = "rental_id", nullable = false)
+	@Schema(description = "ID of the rental to which belongs message", example = "2")
 	private Integer rental_id;
 
 	@Column(name="created_at", columnDefinition = "TIMESTAMP")
 	@CreationTimestamp
+	@Schema(description = "Date when message was created", example = "2025-11-13T16:01:16Z", accessMode = Schema.AccessMode.READ_ONLY)
     private Instant createdAt;
     
     @Column(name="updated_at", columnDefinition = "TIMESTAMP")
     @UpdateTimestamp
+    @Schema(description = "Date when message was updated", example = "2025-11-13T16:01:16Z", accessMode = Schema.AccessMode.READ_ONLY)
     private Instant updatedAt;
 }
