2b555e633c1c2af3e7e72eee6528e64c31b595b5
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-14 07:36:48 +0100

    refactor: :recycle: Added annotations for swagger in exceptions

--- src/main/java/com/openclassrooms/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/openclassrooms/api/exception/GlobalExceptionHandler.java
@@ -6,12 +6,14 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class) 
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ErrorResponse> handleAllExceptions(Exception ex) {
         ErrorResponse error = new ErrorResponse(ex.getMessage());
         return ResponseEntity
@@ -21,6 +23,7 @@ public class GlobalExceptionHandler {
 	
 	// error handling if the route not found	
 	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Map<String, String>> handleNotFound(NoHandlerFoundException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("status", "error");
