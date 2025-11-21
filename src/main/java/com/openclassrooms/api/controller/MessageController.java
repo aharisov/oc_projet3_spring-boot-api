a53ad10094b86dda4f3bd287f1c7e4081d4f8062
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-21 16:31:30 +0100

    refactor: :recycle: Updated message controller for swagger

--- src/main/java/com/openclassrooms/api/controller/MessageController.java
+++ src/main/java/com/openclassrooms/api/controller/MessageController.java
@@ -7,20 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.api.dto.AddMessageDto;
 import com.openclassrooms.api.exception.ErrorResponse;
-import com.openclassrooms.api.model.Message;
 import com.openclassrooms.api.service.MessageService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -30,7 +28,11 @@ public class MessageController {
 	@Autowired
 	private MessageService messageService;
 	
-	@Operation(summary = "Add message")
+	@Operation(
+		summary = "Add message",
+		description = "Requires Bearer JWT. Adds message to the data base.",
+        security = @SecurityRequirement(name = "bearerAuth") 
+	)
 	@ApiResponses(value = {
 			@ApiResponse(
 					responseCode = "200", 
@@ -58,13 +60,10 @@ public class MessageController {
 				    required = true,
 				    content = @Content(
 				    		mediaType = "application/json",
-				    		schema = @Schema(implementation = Message.class)
+				    		schema = @Schema(implementation = AddMessageDto.class)
 				    )
 		    )
-			@RequestBody AddMessageDto data,
-			
-			@Parameter(description = "Bearer token", required = true)
-			@RequestHeader("Authorization") String rawToken
+			@RequestBody AddMessageDto data
 		) {
 		
 		// send message data to the service
