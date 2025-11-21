8cd3b98ddd2d5d7937f9b973128709c5cdee7793
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-21 11:18:16 +0100

    feat: :sparkles: Added mapper config with converting of created_at and updated_at date

---
+++ src/main/java/com/openclassrooms/api/configuration/MapperConfig.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.api.configuration;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.openclassrooms.api.dto.UserInfoDto;
+import com.openclassrooms.api.model.User;
+
+@Configuration
+public class MapperConfig {
+
+	@Bean
+	ModelMapper modelMapperBean() {
+	    ModelMapper modelMapper = new ModelMapper();
+
+	    // Converter for transforming mysql data to the formatted date string	    
+	    Converter<Instant, String> instantToString =
+	    		ctx -> ctx.getSource() == null ? null :
+	                   DateTimeFormatter.ofPattern("yyyy/MM/dd")
+	                   		.withZone(ZoneId.systemDefault())
+                            .format(ctx.getSource());
+
+	    modelMapper.typeMap(User.class, UserInfoDto.class)
+	    	.addMappings(mapper -> {
+	    		mapper.using(instantToString).map(User::getCreated_at, UserInfoDto::setCreated_at);
+                mapper.using(instantToString).map(User::getUpdated_at, UserInfoDto::setUpdated_at);
+            });
+
+	    return modelMapper;
+	}
+}
