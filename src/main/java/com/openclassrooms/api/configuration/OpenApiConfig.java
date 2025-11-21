5018cbae60b2e5e2a6d1ed233a8dbaa75cfb3cc2
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2025-11-21 16:27:17 +0100

    fix: :ambulance: Added OpenApiConfig to enable authentication in the swagger

---
+++ src/main/java/com/openclassrooms/api/configuration/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.openclassrooms.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+// enable authentication in swagger
+@OpenAPIDefinition(
+        info = @Info(title = "Rental API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth") // global security
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@Configuration
+public class OpenApiConfig {
+
+}
